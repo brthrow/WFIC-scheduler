520078d02b02ccc80331829a4279c2cb99568587
Blake Thrower <brthrow@g.clemson.edu>
2017-04-14 13:01:08 -0400

    More boilerplate code

--- Watt Center Schedule/check.php
+++ Watt Center Schedule/check.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WFIC Room Scheduler</title>
+<style>
+	table {
+		width: 100%;
+		border-collapse: collapse;
+	}
+	
+	table, td, th {
+		border: 1px aolid black;
+		padding: 5px;
+	}
+	
+	th {
+		text-align: left;
+	}
+</style>
+</head>
+<body>
+<header>WFIC Room Scheduling Tool</header>
+<br>
+<br>
+<br>
+</body>
+</html>
+
 <?php
 
 
